8cbd83c8c4637d5cc4de50d2517f1727dc1233d7
Ankit Sharma <147038733+Anku7983@users.noreply.github.com>
2025-06-24 08:53:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        console.log(e.target);
+        if(e.target.innerHTML== 'C'){
+            string = "";
+            document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=='='){
+            string = eval(string)
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML=='^2'){
+            string = string*string;
+            string = eval(string)
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML=='^3'){
+            string = string*string*string;
+            string = eval(string)
+            document.querySelector('input').value = string;
+        }
+        else{
+            string = string+e.target.innerHTML;
+            document.querySelector('input').value=string;
+        }
+        
+    })
+
+})
